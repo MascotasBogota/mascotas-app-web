3a2a3110dcd54ab71d3bbd0e7838b067b27a1e66
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2025-05-23 19:46:19 -0500

    implementación de chain of responsability en userController para registro

---
+++ backend/services/userCreationValidation.js
@@ -0,0 +1,27 @@
+import {
+  requiredFieldsHandler,
+  fullNameHandler,
+  validEmailHandler,
+  passwordLengthHandler,
+  strongPasswordHandler,
+  existingUserHandler,
+} from "../handlers/userCreationHandler";
+
+export const userCreationChain = () => {
+  const requiredFields = new requiredFieldsHandler();
+  const fullNameLength = new fullNameHandler();
+  const emailFormat = new validEmailHandler();
+  const passwordLength = new passwordLengthHandler();
+  const passwordStrength = new strongPasswordHandler();
+  const existingUser = new existingUserHandler();
+
+  //Creación de la cadena
+  requiredFields
+    .setNext(fullNameLength)
+    .setNext(emailFormat)
+    .setNext(passwordLength)
+    .setNext(passwordStrength)
+    .setNext(existingUser);
+
+  return requiredFields;
+};
