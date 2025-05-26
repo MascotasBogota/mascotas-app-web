b83262dffbdc3aa3094495ef0c6f7886c3768485
dcifuentesg <dcifuentesg@unal.edu.co>
2025-05-25 19:57:45 -0500

    chore: update dependencies and configure coverage settings for Vitest
    - Added @vitest/coverage-v8 as a dev dependency in package.json.
    - Updated vitest.config.js to streamline coverage configuration, removing unnecessary threshold settings and improving readability.

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -1,30 +1,22 @@
 // vitest.config.js
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: "node", // Usa el entorno de Node.js para tus tests de backend
-    globals: true, // Esto hace que 'vi', 'expect', 'describe', etc. estén disponibles globalmente
+    environment: 'node',
+    globals: true,
     coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "lcov", "html"], // Formatos de reporte de cobertura
+      provider: 'v8', // Específica que use v8
+      reporter: ['text', 'json', 'lcov', 'html'],
       exclude: [
-        "node_modules/",
-        "tests/",
-        "**/*.test.js",
-        "coverage/",
-        "server.js",
-      ],
-      threshold: {
-        global: {
-          branches: 80,
-          functions: 80,
-          lines: 80,
-          statements: 80,
-        },
-      },
+        'node_modules/',
+        'tests/',
+        '**/*.test.js',
+        'coverage/',
+        'server.js'
+      ]
     },
     testTimeout: 10000,
     hookTimeout: 10000,
   },
-});
+})
